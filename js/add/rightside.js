8881f0ff155b9b56e55336e90fd63a6e8472449d
L1ttleQing <820314438@qq.com>
2024-11-29 10:12:39 +0800

    Site updated: 2024-11-29 10:12:38

--- js/add/rightside.js
+++ js/add/rightside.js
@@ -106,24 +106,6 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 
 
-  /*
-  // 复制提醒
-  document.addEventListener("copy",function(e){
-    new Vue({
-      data:function(){
-        this.$notify({
-          title:"哎嘿！复制成功",
-          message:"若要转载请务必保留原文链接！猹分你个瓜！",
-          position: 'bottom-right',
-          offset: 50,
-          showClose: false,
-          type:"success"
-        });
-        return{visible:false}
-      }
-    })
-  })
-  */
 
   document.onkeydown = function () {
     if (window.event && window.event.keyCode == 123) {
